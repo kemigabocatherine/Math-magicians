5ab0c43c5df08265a90292c8b338026432bc9324
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-04-21 15:33:23 +0300

    Add classes to some buttons

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/prefer-stateless-function */
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import './calculator.css';
 
 export default class MyCalculator extends React.Component {
   render() {
@@ -14,22 +15,22 @@ export default class MyCalculator extends React.Component {
           <button type="button">AC</button>
           <button type="button">+/&ndash;</button>
           <button type="button">%</button>
-          <button type="button">&divide;</button>
+          <button type="button" className="operators">&divide;</button>
           <button type="button">7</button>
           <button type="button">8</button>
           <button type="button">9</button>
-          <button type="button">&times;</button>
+          <button type="button" className="operators">&times;</button>
           <button type="button">4</button>
           <button type="button">5</button>
           <button type="button">6</button>
-          <button type="button">&ndash;</button>
+          <button type="button" className="operators">&ndash;</button>
           <button type="button">1</button>
           <button type="button">2</button>
           <button type="button">3</button>
-          <button type="button">+</button>
-          <button type="button">0</button>
+          <button type="button" className="operators">+</button>
+          <button type="button" className="zero">0</button>
           <button type="button">.</button>
-          <button type="button">=</button>
+          <button type="button" className="operators">=</button>
         </div>
       </div>
     );
