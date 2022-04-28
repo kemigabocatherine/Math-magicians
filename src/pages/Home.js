ea18cdc9274ac43cf57319afb1f9d4c22f5eadb8
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-04-28 15:12:45 +0300

    Add the home and qoutes pages

---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <div className="home-title">
+      <h2>Welcome to the Math Magicians Page!</h2>
+    </div>
+
+    <div className="content">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+        accusantium repellat sunt quod reiciendis alias libero molestiae iusto
+        voluptas deleniti quae dolore dolor, voluptate autem? Qui dolorum ab
+        maxime adipisci.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+        accusantium repellat sunt quod reiciendis alias libero molestiae iusto
+        voluptas deleniti quae dolore dolor, voluptate autem? Qui dolorum ab
+        maxime adipisci.
+      </p>
+    </div>
+  </div>
+);
+
+export default Home;
---
+++ src/pages/Quotes.js
@@ -0,0 +1,12 @@
+/* eslint-disable max-len */
+import React from 'react';
+
+const Quotes = () => (
+  <div>
+    <h2>
+      Mathematics knows no races or geographic boundaries; for mathematics, the cultural world is one country. - David Hilbert
+    </h2>
+  </div>
+);
+
+export default Quotes;
